77f4d3ba823eaa191fea2161409a62e22dc42885
Kenneth Shaw <kenneth.shaw@knq.io>
2016-11-23 07:08:33 +0700

    Updating documentation on options

--- opts.go
+++ opts.go
@@ -3,22 +3,22 @@ package envcfg
 // Option is an Envcfg option.
 type Option func(*Envcfg)
 
-// VarName sets the name of the environment variable to get the configuration
-// data from.
+// VarName is an option that sets the name of the environment variable to
+// retrieve the configuration data from.
 func VarName(name string) Option {
 	return func(ec *Envcfg) {
 		ec.envVarName = name
 	}
 }
 
-// ConfigFile is the file path to read data from.
+// ConfigFile is an option that sets the file path to read data from.
 func ConfigFile(path string) Option {
 	return func(ec *Envcfg) {
 		ec.configFile = path
 	}
 }
 
-// KeyFilter adds a key filter.
+// KeyFilter is an option that adds a key filter.
 func KeyFilter(key string, f Filter) Option {
 	return func(ec *Envcfg) {
 		ec.filters[key] = f
